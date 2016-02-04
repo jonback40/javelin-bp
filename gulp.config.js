458c4e7440df8f2b38225edf45e68a6e78dc1eb0
Matt Milburn <me@mattmilburn.com>
2016-02-04 09:25:08 -0600

    Convert tabs to spaces in gulp files

--- gulp.config.js
+++ gulp.config.js
@@ -1,120 +1,120 @@
 module.exports = function() {
-	// Paths
-	var root = './';
-	var temp = root + '.tmp/';
-	var nodeModules = root + 'node_modules/';
-	var build = root + 'build/';
-	var source = root + 'src/';
-	var publicScripts = 'public/scripts/';
-	
-	
-	// Inject Plugin
-	var inject = {
-		options: {
-			addRootSlash: false,
-			ignorePath: [
-				'build/default/',
-				'build/mobile/',
-				'build/responsive/'
-			],
-			removeTags: true
-		}
-	};
-	
-	
-	// Prompt Plugin
-	var prompt = {
-		 inputs: [
-			{
-				type: 'checkbox',
-				name: 'type',
-				message: 'Which type of site are you generating?',
-				choices: [
-					'desktop (default)',
-					'mobile',
-					'responsive'
-				]
-			},
-			{
-				type: 'input',
-				name: 'site',
-				message: 'Javelin site shortname'
-			},
-			{
-				type: 'input',
-				name: 'title',
-				message: 'Project title (or company name)'
-			},
-			{
-				type: 'input',
-				name: 'author',
-				message: 'Author (first and last name)'
-			},
-			{
-				type: 'checkbox',
-				name: 'libs',
-				message: 'Which built-in libraries do you want to include, if any?',
-				choices: [
-					'jquery.javelin.js',
-					'less.canvas-panels.js',
-					'less.dropdown.js',
-					'less.parallax.js',
-					'less.scrollto.js',
-					'less.search.js',
-					'less.simplemodal-video.js'
-				]
-			},
-			{
-				type: 'checkbox',
-				name: 'scripts',
-				message: 'Which local scripts do you want to include, if any?',
-				choices: [
-					'common.js',
-					'index.js'
-				]
-			}
-		],
-		
-		inputsMap: {
-			'desktop (default)':			'default',
-			'mobile':						'mobile',
-			'responsive':					'responsive',
-			
-			'jquery.javelin.js':			publicScripts + 'jquery/javelin.jquery.js',
-			'less.canvas-panels.js':		publicScripts + 'less/less.canvas-panels.min.js',
-			'less.dropdown.js':				publicScripts + 'less/less.dropdown.min.js',
-			'less.search.js':				publicScripts + 'less/less.search.min.js',
-			'less.simplemodal-video.js':	publicScripts + 'less/less.simplemodal-video.min.js',
-			
-			'common.js':					'js/common.min.js',
-			'index.js':						'js/index.min.js'
-		}
-	};
-	
-	
-	// Config
-	var config = {
-		// Paths
-		root: root,
-		temp: temp,
-		nodeModules: nodeModules,
-		build: build,
-		source: source,
-		publicScripts: publicScripts,
-		
-		// Globs
-		styles: 'css/**/*.css',
-		scripts: 'js/**/*.js',
-		images: 'images/**/*.{jpg,jpeg,gif,png,svg}',
-		templates: '*.html',
-		
-		// Acceptable File Types (markup, styles, scripts, images, fonts)
-		acceptedTypes: '**/*.{html,css,less,js,jpg,jpeg,gif,ico,png,svg,eot,ttf,woff,woff2}',
-		
-		// Plugins
-		inject: inject,
-		prompt: prompt
-	};
-	
-	return config;
+    // Paths
+    var root = './';
+    var temp = root + '.tmp/';
+    var nodeModules = root + 'node_modules/';
+    var build = root + 'build/';
+    var source = root + 'src/';
+    var publicScripts = 'public/scripts/';
+    
+    
+    // Inject Plugin
+    var inject = {
+        options: {
+            addRootSlash: false,
+            ignorePath: [
+                'build/default/',
+                'build/mobile/',
+                'build/responsive/'
+            ],
+            removeTags: true
+        }
+    };
+    
+    
+    // Prompt Plugin
+    var prompt = {
+         inputs: [
+            {
+                type: 'checkbox',
+                name: 'type',
+                message: 'Which type of site are you generating?',
+                choices: [
+                    'desktop (default)',
+                    'mobile',
+                    'responsive'
+                ]
+            },
+            {
+                type: 'input',
+                name: 'site',
+                message: 'Javelin site shortname'
+            },
+            {
+                type: 'input',
+                name: 'title',
+                message: 'Project title (or company name)'
+            },
+            {
+                type: 'input',
+                name: 'author',
+                message: 'Author (first and last name)'
+            },
+            {
+                type: 'checkbox',
+                name: 'libs',
+                message: 'Which built-in libraries do you want to include, if any?',
+                choices: [
+                    'jquery.javelin.js',
+                    'less.canvas-panels.js',
+                    'less.dropdown.js',
+                    'less.parallax.js',
+                    'less.scrollto.js',
+                    'less.search.js',
+                    'less.simplemodal-video.js'
+                ]
+            },
+            {
+                type: 'checkbox',
+                name: 'scripts',
+                message: 'Which local scripts do you want to include, if any?',
+                choices: [
+                    'common.js',
+                    'index.js'
+                ]
+            }
+        ],
+        
+        inputsMap: {
+            'desktop (default)':            'default',
+            'mobile':                       'mobile',
+            'responsive':                   'responsive',
+            
+            'jquery.javelin.js':            publicScripts + 'jquery/javelin.jquery.js',
+            'less.canvas-panels.js':        publicScripts + 'less/less.canvas-panels.min.js',
+            'less.dropdown.js':             publicScripts + 'less/less.dropdown.min.js',
+            'less.search.js':               publicScripts + 'less/less.search.min.js',
+            'less.simplemodal-video.js':    publicScripts + 'less/less.simplemodal-video.min.js',
+            
+            'common.js':                    'js/common.min.js',
+            'index.js':                     'js/index.min.js'
+        }
+    };
+    
+    
+    // Config
+    var config = {
+        // Paths
+        root: root,
+        temp: temp,
+        nodeModules: nodeModules,
+        build: build,
+        source: source,
+        publicScripts: publicScripts,
+        
+        // Globs
+        styles: 'css/**/*.css',
+        scripts: 'js/**/*.js',
+        images: 'images/**/*.{jpg,jpeg,gif,png,svg}',
+        templates: '*.html',
+        
+        // Acceptable File Types (markup, styles, scripts, images, fonts)
+        acceptedTypes: '**/*.{html,css,less,js,jpg,jpeg,gif,ico,png,svg,eot,ttf,woff,woff2}',
+        
+        // Plugins
+        inject: inject,
+        prompt: prompt
+    };
+    
+    return config;
 };
--- gulpfile.js
+++ gulpfile.js
@@ -28,81 +28,81 @@ var params;
 
 // Before we start the build process, prompt the user to provide some input
 function buildPrompt() {
-	return gulp.src(config.root)
-		.pipe($.prompt.prompt(config.prompt.inputs, saveResponse));
-	
-	function saveResponse(response) {
-		// Go through each response property (they're either a string or array)
-		// ...if string, find matching key and replace with corresponding value
-		// ...if array,  loop through list, find matching key and replace with corresponding value
-		for (var prop in response) {
-			if (response.hasOwnProperty(prop)) {
-				var value = response[prop],
-					len = value.length;
-				
-				if (value.length) {
-					for (var i = 0; i < len; i++) {
-						response[prop][i] = getMappedParam(value[i]);
-					}
-				} else {
-					response[prop] = getMappedParam(value);
-				}
-			}
-		}
-		
-		response.date = dateString();
-		
-		params = response;
-	}
+    return gulp.src(config.root)
+        .pipe($.prompt.prompt(config.prompt.inputs, saveResponse));
+    
+    function saveResponse(response) {
+        // Go through each response property (they're either a string or array)
+        // ...if string, find matching key and replace with corresponding value
+        // ...if array,  loop through list, find matching key and replace with corresponding value
+        for (var prop in response) {
+            if (response.hasOwnProperty(prop)) {
+                var value = response[prop],
+                    len = value.length;
+                
+                if (value.length) {
+                    for (var i = 0; i < len; i++) {
+                        response[prop][i] = getMappedParam(value[i]);
+                    }
+                } else {
+                    response[prop] = getMappedParam(value);
+                }
+            }
+        }
+        
+        response.date = dateString();
+        
+        params = response;
+    }
 }
 
 
 // Clean out the build directory before we start another build process
 function cleanTask() {
-	return del(config.build);
+    return del(config.build);
 }
 
 
 // Inject dependencies into the HTML template files
 function injectTask() {
-	return gulp.src(config.build + '/' + params.type + '/' + config.templates)
-		.pipe($.inject(
-			gulp.src(config.build + '/' + params.type + '/' + config.styles, { read: false }),
-			config.inject.options
-		))
-		
-		// TODO: Need to fix. Using 'params.libs' as an array of strings does not work here
-		.pipe($.inject(
-			gulp.src(params.libs, { read: false }),
-			config.inject.options
-		))
-		.pipe(gulp.dest(config.build + '/' + params.type));
+    return gulp.src(config.build + '/' + params.type + '/' + config.templates)
+        .pipe($.inject(
+            gulp.src(config.build + '/' + params.type + '/' + config.styles, { read: false }),
+            config.inject.options
+        ))
+        
+        // TODO: Need to fix. Using 'params.libs' as an array of strings does not work here
+        .pipe($.inject(
+            gulp.src(params.libs, { read: false }),
+            config.inject.options
+        ))
+        .pipe(gulp.dest(config.build + '/' + params.type));
 }
 
 
 // Interpolate input data into the HTML template files
 function interpolateTemplatesTask() {
-	var src = gulp.src(config.build + '/' + params.type + '/' + config.templates);
-	
-	return interpolate(src).pipe(gulp.dest(config.build + '/' + params.type));
+    var src = gulp.src(config.build + '/' + params.type + '/' + config.templates);
+    
+    return interpolate(src).pipe(gulp.dest(config.build + '/' + params.type));
 }
 
 
 // Interpolate input data into the LESS files (compiler and config)
 function interpolateLessTask() {
-	var src = gulp.src([
-		config.build + '/' + params.type + '/css/less/compiler.less',
-		config.build + '/' + params.type + '/css/less/config.less'
-	]);
-	
-	return interpolate(src).pipe(gulp.dest(config.build + '/' + params.type + '/css/less/'));
+    var src = gulp.src([
+        config.build + '/' + params.type + '/css/less/compiler.less',
+        config.build + '/' + params.type + '/css/less/config.less'
+    ]);
+    
+    return interpolate(src).pipe(gulp.dest(config.build + '/' + params.type + '/css/less/'));
 }
 
 
 // Put HTML Templates into the build directory
 function templatesTask() {
-	return gulp.src(config.source + params.type + '/' + config.acceptedTypes)
-		.pipe(gulp.dest(config.build + '/' + params.type));
+    return gulp.src(config.source + params.type + '/' + config.acceptedTypes)
+        .pipe(gulp.dest(config.build + '/' + params.type));
 }
 
 
@@ -111,24 +111,24 @@ function templatesTask() {
 
 // Return the current month and year in a readable, string format
 function dateString() {
-	var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
-	var date = new Date();
-	
-	return months[date.getMonth()] + ' ' + date.getFullYear();
+    var months = ['January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    var date = new Date();
+    
+    return months[date.getMonth()] + ' ' + date.getFullYear();
 }
 
 
 // Return the param value from the 'inputsMap' in the config file. This allows us to manage checkboxes with different labels and values.
 function getMappedParam(param) {
-	return config.prompt.inputsMap[param];
+    return config.prompt.inputsMap[param];
 }
 
 
 // Interpolate our 'params' data into a file stream
 function interpolate(src) {
-	return src
-		.pipe($.replace(/\{\{SITE\}\}/g, params.site))
-		.pipe($.replace(/\{\{TITLE\}\}/g, params.title))
-		.pipe($.replace(/\{\{AUTHOR\}\}/g, params.author))
-		.pipe($.replace(/\{\{DATE\}\}/g, params.date));
+    return src
+        .pipe($.replace(/\{\{SITE\}\}/g, params.site))
+        .pipe($.replace(/\{\{TITLE\}\}/g, params.title))
+        .pipe($.replace(/\{\{AUTHOR\}\}/g, params.author))
+        .pipe($.replace(/\{\{DATE\}\}/g, params.date));
 }
